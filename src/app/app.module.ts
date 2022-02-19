57c87f7de50bd29c4aa78741e527aa77d4e7663e
Herbert Pro <hprocastillo@gmail.com>
2022-02-19 07:34:30 -0500

    commit inicial, about me, skills, resumen and one project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NavbarComponent} from './components/layout/navbar/navbar.component';
+import {LeftColumnComponent} from './components/layout/left-column/left-column.component';
+import {MainColumnComponent} from './components/layout/main-column/main-column.component';
+import { HomeComponent } from './components/sections/home/home.component';
+import { AboutComponent } from './components/sections/about/about.component';
+import { SkillsComponent } from './components/sections/skills/skills.component';
+import { ProjectsComponent } from './components/sections/projects/projects.component';
+import { ResumeComponent } from './components/sections/resume/resume.component';
+import { ContactComponent } from './components/sections/contact/contact.component';
+import { ProfileAvatarComponent } from './components/layout/left-column/profile-avatar/profile-avatar.component';
+import { MenuDesktopComponent } from './components/layout/left-column/menu-desktop/menu-desktop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LeftColumnComponent,
+    MainColumnComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ResumeComponent,
+    ContactComponent,
+    ProfileAvatarComponent,
+    MenuDesktopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/layout/main-column/main-column.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-column',
+  templateUrl: './main-column.component.html',
+  styleUrls: ['./main-column.component.scss']
+})
+export class MainColumnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
